3bf2394424b3922fa971d6141f532e6bc1015b43
Roger Guillet <roger.guillet@workbooster.ch>
2015-06-07 10:35:32 +0200

    Added a logic to get a Synery symbol name for SyneryTypes or primitive .NET types and particularized some comments.

--- src/InterfaceBooster.Common.Interfaces/Utilities/SyneryLanguage/TypeHelper.cs
+++ src/InterfaceBooster.Common.Interfaces/Utilities/SyneryLanguage/TypeHelper.cs
@@ -1,4 +1,6 @@
-﻿using InterfaceBooster.Common.Interfaces.SyneryLanguage.Model.Context;
+﻿using InterfaceBooster.Common.Interfaces.ErrorHandling;
+using InterfaceBooster.Common.Interfaces.SyneryLanguage.Model.Context;
+using InterfaceBooster.Common.Interfaces.SyneryLanguage.Model.SyneryTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,8 +27,17 @@ namespace InterfaceBooster.Common.Interfaces.Utilities.SyneryLanguage
         public static SyneryType CHAR_TYPE = new SyneryType(CHAR_DOTNET_TYPE);
         public static SyneryType DATETIME_TYPE = new SyneryType(DATETIME_DOTNET_TYPE);
 
+        /// <summary>
+        /// A list of all supported primitive .NET CLR types.
+        /// </summary>
         public static Type[] SUPPORTED_DOTNET_TYPES = new Type[] { STRING_DOTNET_TYPE, BOOL_DOTNET_TYPE, INT_DOTNET_TYPE, DECIMAL_DOTNET_TYPE, DOUBLE_DOTNET_TYPE, CHAR_DOTNET_TYPE, DATETIME_DOTNET_TYPE };
 
+        /// <summary>
+        /// Gets a SyneryType. If the requested type is primitive CLR type a static SyneryType from a constant (e.g. STRING_TYPE) is returned.
+        /// </summary>
+        /// <param name="type">The underlying .NET CLR type.</param>
+        /// <param name="name">An optional name for complex synery types (e.g. a Record Type)</param>
+        /// <returns></returns>
         public static SyneryType GetSyneryType(Type type, string name = null)
         {
             if (type == typeof(string)) return STRING_TYPE;
@@ -40,6 +51,50 @@ namespace InterfaceBooster.Common.Interfaces.Utilities.SyneryLanguage
             return new SyneryType(type, name);
         }
 
+        /// <summary>
+        /// Gets the symbol name (e.g. "STRING") of the Synery type 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetSyneryTypeSymbol(Type type)
+        {
+            if (type == typeof(string)) return "STRING";
+            if (type == typeof(bool)) return "BOOL";
+            if (type == typeof(int)) return "INT";
+            if (type == typeof(decimal)) return "DECIMAL";
+            if (type == typeof(double)) return "DOUBLE";
+            if (type == typeof(char)) return "CHAR";
+            if (type == typeof(DateTime)) return "DATETIME";
+            
+            throw new SyneryException(String.Format(
+                "The given .NET CLR type '{0}' is unknown as Synery Type.", type.FullName));
+        }
+
+        /// <summary>
+        /// Gets the symbol name of a primitive type (e.g. "STRING") or complex type (e.g. "Person" for a "#Person" Record Type).
+        /// </summary>
+        /// <param name="syneryType"></param>
+        /// <returns></returns>
+        public static string GetSyneryTypeSymbol(SyneryType syneryType)
+        {
+            if (String.IsNullOrEmpty(syneryType.Name))
+            {
+                // it seems to be a primitive type .NET CLR type
+                return GetSyneryTypeSymbol(syneryType.UnterlyingDotNetType);
+            }
+            else
+            {
+                // it seems to be a complex type (e.g. Record Type)
+                return syneryType.Name;
+            }
+        }
+
+        /// <summary>
+        /// Checks whether the given SyneryType is a primitive .NET CLR type.
+        /// An opposite of a primitive type for example would be a Record Type.
+        /// </summary>
+        /// <param name="syneryType"></param>
+        /// <returns></returns>
         public static bool IsPrimitiveType(SyneryType syneryType)
         {
             return SUPPORTED_DOTNET_TYPES.Contains(syneryType.UnterlyingDotNetType);
