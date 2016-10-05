61bfb3a21ceb4d742ca5f4eb1b2c39be411d0499
Roger Guillet <roger.guillet@workbooster.ch>
2016-10-05 13:37:59 +0200

    Started implementing an interpretation client for getting connection declaration information.

---
+++ src/InterfaceBooster.SyneryLanguage/Interpretation/ConnectTaskDeclarationInterpretationClient.cs
@@ -0,0 +1,121 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceBooster.SyneryLanguage.Common;
+using InterfaceBooster.SyneryLanguage.Interpretation.BaseLanguage.Functions;
+using InterfaceBooster.Common.Interfaces.SyneryLanguage;
+using InterfaceBooster.Common.Interfaces.SyneryLanguage.Model.Context;
+using InterfaceBooster.Common.Interfaces.ProviderPlugin.Control;
+
+namespace InterfaceBooster.SyneryLanguage.Interpretation
+{
+    public class ConnectTaskDeclarationInterpretationClient : ISyneryClient<IList<ProviderPluginConnectTask>>
+    {
+        #region PROPERTIES
+
+        public IInterpretationController Controller { get; private set; }
+
+        public ISyneryMemory Memory { get; set; }
+
+        public IAntlrErrorListener<int> LexerErrorListener { get; set; }
+
+        public IAntlrErrorListener<IToken> ParserErrorListener { get; set; }
+
+        #endregion
+
+        #region PUBLIC METHODS
+
+        public ConnectTaskDeclarationInterpretationClient(ISyneryMemory memory, IAntlrErrorListener<int> lexerErrorListener = null, IAntlrErrorListener<IToken> parserErrorListener = null)
+        {
+            Memory = memory;
+            LexerErrorListener = lexerErrorListener;
+            ParserErrorListener = parserErrorListener;
+
+            IInterpreterFactory factory = InterpreterFactory.GetDefaultInterpreterFactory();
+            factory.SetInterpreter(new SyneryFunctionBlockInterpreter());
+
+            Controller = new InterpretationController(factory, memory);
+        }
+
+        #region IMPLEMENTATION OF ISyneryClient
+
+        /// <summary>
+        /// Gets a list of provider plugin connections from the given synery code with all the information that is needed to establish a connection.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="includeCode"></param>
+        /// <returns></returns>
+        public IList<ProviderPluginConnectTask> Run(string code, IDictionary<string, string> includeCode = null)
+        {
+            // extract the functions and add them to the memory
+
+            return ExtractConnectTaskData(code, includeCode);
+        }
+
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #endregion
+
+
+
+        #region INTERNAL METHODS
+
+        private IList<ProviderPluginConnectTask> ExtractConnectTaskData(string code, IDictionary<string, string> includeCode = null)
+        {
+            List<ProviderPluginConnectTask> listOfConnectTaskDataObjects = new List<ProviderPluginConnectTask>();
+
+            // extract connect task data from main code
+            listOfConnectTaskDataObjects.AddRange(ExtractConnectTaskDataFromCode(code));
+
+            if (includeCode != null)
+            {
+                // extract connect task data from included code files
+
+                foreach (var item in includeCode)
+                {
+                    listOfConnectTaskDataObjects.AddRange(ExtractConnectTaskDataFromCode(item.Value));
+                }
+            }
+
+            return listOfConnectTaskDataObjects;
+        }
+
+        private IList<ProviderPluginConnectTask> ExtractConnectTaskDataFromCode(string code)
+        {
+            List<ProviderPluginConnectTask> listOfConnectTaskDataObjects = new List<ProviderPluginConnectTask>();
+
+            SyneryParser.ProgramContext programContexts = ParserHelper.GetProgramAstFromCode(code, LexerErrorListener, ParserErrorListener);
+
+            if (programContexts != null)
+            {
+                // load connect statements that are nested like that:
+                // programUnit > providerPluginStatement > providerPluginConnectStatement
+                foreach (var programUnitContext in programContexts.GetRuleContexts<SyneryParser.ProgramUnitContext>())
+                {
+                    foreach (var providerPluginStatementContext in programUnitContext.GetRuleContexts<SyneryParser.ProviderPluginStatementContext>())
+                    {
+                        foreach (var connectStatementContext in providerPluginStatementContext.GetRuleContexts<SyneryParser.ProviderPluginConnectStatementContext>())
+                        {
+                            ProviderPluginConnectTask connectTask = Controller.Interpret<SyneryParser.ProviderPluginConnectStatementContext, ProviderPluginConnectTask>(connectStatementContext);
+                            listOfConnectTaskDataObjects.Add(connectTask);
+                        }
+                        
+                    }
+                    
+                }
+            }
+
+            return listOfConnectTaskDataObjects;
+        }
+
+        #endregion
+    }
+}
---
+++ src/InterfaceBooster.Test.SyneryLanguage/Interpretation/ConnectTaskDeclarationInterpretationClient_Test/All_Connections_Are_Extracted.cs
@@ -0,0 +1,68 @@
+﻿using InterfaceBooster.Common.Interfaces.Broadcasting;
+using InterfaceBooster.Common.Interfaces.InterfaceDefinition.Data;
+using InterfaceBooster.Common.Interfaces.ProviderPlugin.Interfaces;
+using InterfaceBooster.Common.Interfaces.SyneryLanguage.Model.Context;
+using InterfaceBooster.Core.ProviderPlugins;
+using InterfaceBooster.SyneryLanguage.Interpretation;
+using InterfaceBooster.SyneryLanguage.Model.Context;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBooster.Test.SyneryLanguage.Interpretation.ConnectTaskDeclarationInterpretationClient_Test
+{
+    [TestFixture]
+    public class All_Connections_Are_Extracted
+    {
+        private ConnectTaskDeclarationInterpretationClient _Client;
+        private ISyneryMemory _SyneryMemory;
+        private string _Code;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            string solutionDirectoryPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+            string pluginMainDirectoryPath = Path.Combine(solutionDirectoryPath, @"_TestData\InterfaceBooster\ProviderPluginDirectory");
+
+            ProviderPluginInstanceReference simpleDummyReference = new ProviderPluginInstanceReference();
+            simpleDummyReference.SyneryIdentifier = "DummyOne";
+            simpleDummyReference.IdPlugin = new Guid("485eccb4-3920-4dc3-9ed4-27f65e8b3c91");
+            simpleDummyReference.PluginName = "ReferencePluginName";
+            simpleDummyReference.IdPluginInstance = new Guid("58e6a1b5-9eb5-45ab-8c9b-8b267e2d09e8");
+            simpleDummyReference.PluginInstanceName = "ReferencePluginInstanceName";
+
+            // activate provider plugin instance
+
+            IProviderPluginManager providerPluginManager = new ProviderPluginManager(pluginMainDirectoryPath);
+            providerPluginManager.Activate(simpleDummyReference);
+
+            _SyneryMemory = new SyneryMemory(null, new DefaultBroadcaster(), providerPluginManager, null);
+            _Client = new ConnectTaskDeclarationInterpretationClient(_SyneryMemory);
+            _Code = @"
+CONNECT ""DummyOne"" 
+    AS \\Connections\DummyConnection
+    SET (
+        Database.Connection.Server = ""Testserver"",
+        Database.Connection.Database = ""TestDb"",
+        Database.Connection.User = ""TestUser"",
+        Database.Connection.Password = ""TestPassword"",
+        Proffix.Tables.ShowAdditionalTables = TRUE,
+        Proffix.Tables.ShowSystemTables = TRUE
+    ) 
+END
+";
+        }
+
+        [Test]
+        public void Loading_RecordTypes_Works()
+        {
+            var listOfConnectTasks = _Client.Run(_Code);
+
+            Assert.AreEqual(1, listOfConnectTasks.Count);
+        }
+    }
+}
